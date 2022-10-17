8b7f8e945b69c32f4c96ae1f3dd328c9a2717ba8
AlbertFrancisco <albertfr2021@outlook.es>
2022-10-17 17:07:04 +0200

    .

--- app/src/main/java/com/example/sudoku/MainActivity.java
+++ app/src/main/java/com/example/sudoku/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.sudoku;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ArrayList<String> paraules=new ArrayList<>();
+        for(int i=0;i<10;i++){
+            paraules.add("Linia "+Integer.toString(i));
+        }
+
+        Spinner spinnerMostra=new Spinner(this);
+        ArrayAdapter<String> adaptador = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, paraules);
+        adaptador.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerMostra.setAdapter(adaptador);
+
+        
+
+
     }
 }
